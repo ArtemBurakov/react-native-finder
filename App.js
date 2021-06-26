ca4fafd7ea1fe0ab09895cb0a6378a18be7b06fd
Artem Burakov <artem000.burakov@gmail.com>
2021-06-26 15:33:20 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+import MapView from 'react-native-maps';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: '100%',
+    width: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export default App;
